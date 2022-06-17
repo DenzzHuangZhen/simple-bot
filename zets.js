dbe675361b1c4d0651b929bd8fa1984f042e801d
imBimbot77 <107393963+imBimbot77@users.noreply.github.com>
2022-06-17 20:13:00 +0700

    Update zets.js

--- zets.js
+++ zets.js
@@ -28,8 +28,8 @@ const setting = JSON.parse(fs.readFileSync('./apikey.json'))
 
 //limit
 limitawal = '100'
-botname = 'ZETS - MD'
-wm = '© ZETS - MD'
+botname = 'Xinzuo Multi-Perangkat'
+wm = 'Telekomunikasi Indonesia'
 
 // read database
 let tebaklagu = db.data.game.tebaklagu = []
